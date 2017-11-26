fd8c8a41b4e2bdfed9ce35bd0a2d447917359f8d
Balogh Dávid <balogh.david1015@gmail.com>
2017-11-26 19:53:27 +0100

    changed i in the for loop to roll

--- bowling.py
+++ bowling.py
@@ -5,29 +5,29 @@ def score(game):
     spare = '/'
     strike = 'x'
     in_first_half = True
-    for i in range(len(game)):
-        if game[i] == spare:
+    for roll in range(len(game)):
+        if game[roll] == spare:
             result += 10 - last
         else:
-            result += get_value(game[i])
-        if frame < 10  and get_value(game[i]) == 10:
-            if game[i] == spare:
-                result += get_value(game[i+1])
-            elif game[i].lower() == strike:
-                result += get_value(game[i+1])
+            result += get_value(game[roll])
+        if frame < 10  and get_value(game[roll]) == 10:
+            if game[roll] == spare:
+                result += get_value(game[roll+1])
+            elif game[roll].lower() == strike:
+                result += get_value(game[roll+1])
                 frame += 1
-                if game[i+2] == spare:
-                    result += 10 - get_value(game[i+1])
+                if game[roll+2] == spare:
+                    result += 10 - get_value(game[roll+1])
                 else:
-                    result += get_value(game[i+2])
+                    result += get_value(game[roll+2])
         # your last hit
-        last = get_value(game[i])
+        last = get_value(game[roll])
         if in_first_half == True:
             in_first_half = False
         elif not in_first_half:
             in_first_half = True
             frame += 1
-        if game[i].lower() == strike:
+        if game[roll].lower() == strike:
             in_first_half = True
     return result
 
