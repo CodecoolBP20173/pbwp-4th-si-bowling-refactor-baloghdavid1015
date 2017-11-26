ed9d69463419887d15ba1eaefc7ae9d32dc6de0b
Balogh Dávid <balogh.david1015@gmail.com>
2017-11-26 19:36:10 +0100

    added strike, spare

--- bowling.py
+++ bowling.py
@@ -1,6 +1,9 @@
 def score(game):
     result = 0
     frame = 1
+    global spare, strike
+    spare = '/'
+    strike = 'x'
     in_first_half = True
     for i in range(len(game)):
         if game[i] == '/':
@@ -8,31 +11,30 @@ def score(game):
         else:
             result += get_value(game[i])
         if frame < 10  and get_value(game[i]) == 10:
-            if game[i] == '/':
+            if game[i] == spare:
                 result += get_value(game[i+1])
-            elif game[i].lower() == 'x':
+            elif game[i].lower() == strike:
                 result += get_value(game[i+1])
-                if game[i+2] == '/':
+                frame += 1
+                if game[i+2] == spare:
                     result += 10 - get_value(game[i+1])
                 else:
                     result += get_value(game[i+2])
         last = get_value(game[i])
-        if not in_first_half:
-            frame += 1
         if in_first_half == True:
             in_first_half = False
-        else:
-            in_first_half = True
-        if game[i].lower() == 'x':
+        elif not in_first_half:
             in_first_half = True
             frame += 1
+        if game[i].lower() == strike:
+            in_first_half = True
     return result
 
 def get_value(char):
     try:
         return int(char)
     except ValueError:
-        if char.lower() == 'x' or char == "/":
+        if char.lower() == strike or char == spare:
             return 10
         elif char == '-':
             return 0
